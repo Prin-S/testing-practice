5de27a396ec1102b1b03f8cabb3cf3f9aeb00ae9
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-11-02 17:12:51 +0700

    Added a test for reverseString().
    Added: a test for reverseString() which takes a string and returns it reversed.

--- testing-practice.js
+++ testing-practice.js
@@ -2,4 +2,14 @@ function capitalize(text) {
     return text[0].toUpperCase() + text.slice(1);
 }
 
-export { capitalize };
+function reverseString(text) {
+    let reversed = '';
+    
+    for (let i = text.length - 1; i >= 0; i--) {
+        reversed += text[i];
+    }
+
+    return reversed;
+}
+
+export { capitalize, reverseString };
--- testing-practice.test.js
+++ testing-practice.test.js
@@ -1,6 +1,11 @@
-import { capitalize } from './testing-practice.js';
+import { capitalize, reverseString } from './testing-practice.js';
 
 test('capitalize the first letter', () => {
     expect(capitalize('test')).toBe('Test');
     expect(capitalize('testing practice')).toBe('Testing practice');
+});
+
+test('reverse a string', () => {
+    expect(reverseString('test')).toBe('tset');
+    expect(reverseString('The quick brown fox jumps over the lazy dog')).toBe('god yzal eht revo spmuj xof nworb kciuq ehT');
 });
