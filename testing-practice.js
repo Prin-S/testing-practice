9fbf0bf811d8a96ec446bebc8d9f37c226ead495
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-11-02 21:40:28 +0700

    Added a test for caesarCipher().
    Added: a test for caesarCipher() which takes a string and a shift factor and returns it with each character “shifted”.

--- testing-practice.js
+++ testing-practice.js
@@ -19,4 +19,39 @@ const calculator = {
     divide: (a, b) => a / b
 }
 
-export { capitalize, reverseString, calculator };
+function caesarCipher(text, shift) { // shift is the amount in ASCII decimal value to be moved.
+    if (shift < -26 || shift > 26) {
+        return 'The shift factor needs to be from -26 to 26.';
+    }
+
+    let encoded = '';
+    let wrapAround = 0; 
+    
+    if (shift < 0) {
+        shift += 26; // Make shift positive.
+    }
+
+    for (let i = 0; i <= text.length - 1; i++) {
+        if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) { // The ASCII decimal value for A-Z is 65 to 90.
+            if (text.charCodeAt(i) + shift <= 90) {
+                encoded += String.fromCharCode(text.charCodeAt(i) + shift);
+            } else { // Wrap around if the value is over 90.
+                wrapAround = text.charCodeAt(i) + shift - 91; // An extra -1 is needed to prevent the value from going off by 1.
+                encoded += String.fromCharCode(65 + wrapAround);
+            }
+        } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) { // The ASCII decimal value for a-z is 97 to 122.
+            if (text.charCodeAt(i) + shift <= 122) {
+                encoded += String.fromCharCode(text.charCodeAt(i) + shift);
+            } else { // Wrap around if the value is over 122.
+                wrapAround = text.charCodeAt(i) + shift - 123; // An extra -1 is needed to prevent the value from going off by 1.
+                encoded += String.fromCharCode(97 + wrapAround);
+            }
+        } else { // For other characters
+            encoded += text[i];
+        }
+    }
+
+    return encoded;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher };
--- testing-practice.test.js
+++ testing-practice.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from './testing-practice.js';
+import { capitalize, reverseString, calculator, caesarCipher } from './testing-practice.js';
 
 test('capitalize the first letter', () => {
     expect(capitalize('test')).toBe('Test');
@@ -7,7 +7,7 @@ test('capitalize the first letter', () => {
 
 test('reverse a string', () => {
     expect(reverseString('test')).toBe('tset');
-    expect(reverseString('The quick brown fox jumps over the lazy dog')).toBe('god yzal eht revo spmuj xof nworb kciuq ehT');
+    expect(reverseString('The quick brown fox jumps over the lazy dog.')).toBe('.god yzal eht revo spmuj xof nworb kciuq ehT');
 });
 
 describe('calculate two numbers', () => {
@@ -25,5 +25,47 @@ describe('calculate two numbers', () => {
 
     test('divide', () => {
         expect(calculator.divide(9, 3)).toBe(3);
+    });
+});
+
+describe('Caesar cipher', () => {
+    test(`caesarCipher('xyz', 3)`, () => {
+        expect(caesarCipher('xyz', 3)).toBe('abc');
+    });
+
+    test(`caesarCipher('HeLLo', 3)`, () => {
+        expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+    });
+
+    test(`caesarCipher('Hello, World!', 3)`, () => {
+        expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+    });
+
+    test(`caesarCipher('The quick brown fox jumps over the lazy dog.', 15)`, () => {
+        expect(caesarCipher('The quick brown fox jumps over the lazy dog.', 15)).toBe('Iwt fjxrz qgdlc udm yjbeh dktg iwt apon sdv.');
+    });
+
+    test(`caesarCipher('Hello, World!', 0)`, () => {
+        expect(caesarCipher('Hello, World!', 0)).toBe('Hello, World!');
+    });
+
+    test(`caesarCipher('Hello, World!', 26)`, () => {
+        expect(caesarCipher('Hello, World!', 26)).toBe('Hello, World!');
+    });
+
+    test(`caesarCipher('Hello, World!', 24)`, () => {
+        expect(caesarCipher('Hello, World!', 24)).toBe('Fcjjm, Umpjb!');
+    });
+
+    test(`caesarCipher('Hello, World!', -2)`, () => {
+        expect(caesarCipher('Hello, World!', -2)).toBe('Fcjjm, Umpjb!');
+    });
+
+    test('shiftFactor under -26', () => {
+        expect(caesarCipher('Hello, World!', -88)).toBe('The shift factor needs to be from -26 to 26.');
+    });
+
+    test('shiftFactor over 26', () => {
+        expect(caesarCipher('Hello, World!', 99)).toBe('The shift factor needs to be from -26 to 26.');
     });
 });
